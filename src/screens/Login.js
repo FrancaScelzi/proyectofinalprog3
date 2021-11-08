2301c5762a5d420ecd190863af41a0b7b1cf7e17
FlorenciaVillaverde <florvillaverde01@gmail.com>
2021-11-08 08:37:09 -0300

    UI Login/Register

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,71 +1,98 @@
-import React, { Component }  from "react";
-import { Text, View, StyleSheet, TextInput, TouchableOpacity} from "react-native";
-import { auth } from '../firebase/config'
+import React, { Component } from "react";
+import {Text, View, StyleSheet, TextInput, TouchableOpacity, Image} from "react-native";
+import { auth } from "../firebase/config";
 
-export default class Login extends Component{
-    constructor (props){
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        }
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  onLogin() {
+    if (this.state.email !== "" && this.state.password !== "") {
+      this.props.handleLogin(this.state.email, this.state.password);
+    } else {
+      console.log("¡Completar los campos!");
     }
+  }
 
-    onLogin(){
-        if (this.state.email !== "" && this.state.password !== ""){
-                this.props.handleLogin(this.state.email, this.state.password)
-            }
-         else {
-             console.log("¡Completar los campos!")
-         }
-        }
-
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text style={styles.text}> Login </Text>
-                <TextInput
-                    style={styles.field}
-                    keyboardType= 'email-address'
-                    placeholder= 'Email'
-                    onChangeText= {text => this.setState({email:text})}
-                />
-                <TextInput
-                    style={styles.field}
-                    keyboardType='number-pad'
-                    placeholder="Password"
-                    secureTextEntry={true}
-                    onChangeText={text => this.setState({ password: text })}
-                />
-                <TouchableOpacity style = {styles.button} onPress= {() => this.onLogin()}>
-                    <Text style = {styles.text}>Login</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Image
+            source={require("../../assets/appLogo.png")}
+            style={styles.appLogo}
+            resizeMode={"contain"}
+          />
+        </View>
+        <TextInput
+          style={styles.field}
+          keyboardType="email-address"
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+        <TextInput
+          style={styles.field}
+          keyboardType="number-pad"
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => this.onLogin()}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+        <View>
+          <TouchableOpacity onPress={() => console.log("Navegar al registro")}>
+            <Text style={styles.textBtn}>¿No tenes cuenta? Registrate!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    field: {
-        width: '50%',
-        backgroundColor: "#ced4da",
-        color: '#212529',
-        padding: 10,
-        margin: 10
-    },
-    button: {
-        margin: 10,
-        width: '20%',
-        backgroundColor: "#ced4da",
-        alignItems: "center",
-    },
-    text: {
-        color: '#212529',
-        fontSize: 20,
-        margin: 10
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  field: {
+    width: "70%",
+    height: 50,
+    backgroundColor: "#FAFAFA",
+    borderRadius: 5,
+    borderColor: "gray",
+    borderWidth: 0.1,
+    padding: 10,
+    margin: 10,
+  },
+  button: {
+    margin: 10,
+    width: "20%",
+    alignItems: "center",
+    backgroundColor: "#CCD5AE",
+    borderRadius: 5,
+    marginTop: 25,
+    width: 171,
+    height: 45,
+  },
+  text: {
+    color: "#212529",
+    fontSize: 20,
+    margin: 10,
+  },
+  textBtn: {
+    color: "#212529",
+    fontSize: 20,
+    paddingTop: 40
+  },
+  appLogo: {
+    height: 200,
+    width: 200,
+  },
+});
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Text, TextInput, TouchableOpacity, View, StyleSheet} from 'react-native';
+import { Text, TextInput, TouchableOpacity, View, StyleSheet, Image} from 'react-native';
 import { auth } from '../firebase/config';
 
 export default class Register extends Component {
@@ -24,7 +24,13 @@ onRegister(){
 render(){
     return (
     <View style={styles.container}>
-        <Text style={styles.text}>Registro</Text>
+        <View>
+          <Image
+            source={require("../../assets/appLogo.png")}
+            style={styles.appLogo}
+            resizeMode={"contain"}
+          />
+        </View>
         <TextInput
             style={styles.field}
             keyboardType="default"
@@ -56,24 +62,36 @@ render(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: "white",
     },
     field: {
-        width: '50%',
-        backgroundColor: "#ced4da",
-        color: '#212529',
+        width: '70%',
+        height: 50,
+        backgroundColor: "#FAFAFA",
+        borderRadius: 5,
+        borderColor: "gray",
+        borderWidth: 0.1,
         padding: 10,
-        margin: 10
+        margin: 10,
     },
     button: {
         margin: 10,
-        width: '20%',
-        backgroundColor: "#ced4da",
+        width: "20%",
         alignItems: "center",
+        backgroundColor: "#CCD5AE",
+        borderRadius: 5,
+        marginTop: 25,
+        width: 171,
+        height: 45,
     },
     text: {
         color: '#212529',
         fontSize: 20,
         margin: 10
-    }
+    },
+    appLogo: {
+        height: 200,
+        width: 200,
+      },
 })
