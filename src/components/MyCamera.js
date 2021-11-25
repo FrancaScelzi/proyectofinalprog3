f5668991a508b1c4374c5189b59ca719c85c6ddc
Florencia Villaverde <florvillaverde01@gmail.com>
2021-11-24 23:48:00 -0300

    codigo ordenado parte camara

--- src/components/MyCamera.js
+++ src/components/MyCamera.js
@@ -1,164 +1,156 @@
-import React, {Component} from 'react';
-import {Camera} from 'expo-camera';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {storage} from '../firebase/config'
+import React, { Component } from "react";
+import { Camera } from "expo-camera";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { storage } from "../firebase/config";
 
-export default class MyCamera extends React.Component{
-    constructor(props){
-        super(props);
-        this.camera; // Variable vacía
-        this.state = {
-            photo: '',
-            permission: false
-        }
-    }
+export default class MyCamera extends React.Component {
+  constructor(props) {
+    super(props);
+    this.camera; // Variable vacía
+    this.state = {
+      photo: "",
+      permission: false,
+    };
+  }
 
-    componentDidMount(){
-        Camera.requestCameraPermissionsAsync()
-        .then(response => {
-            console.log(response);
+  componentDidMount() {
+    Camera.requestCameraPermissionsAsync().then((response) => {
+      console.log(response);
+      this.setState({
+        permission: response.granted,
+      });
+    });
+  }
+
+  takePicture() {
+    if (!this.camera) return;
+    this.camera.takePictureAsync().then((photo) => {
+      this.setState({
+        photo: photo.uri,
+      });
+    });
+  }
+
+  uploadImage() {
+    fetch(this.state.photo)
+      .then((res) => {
+        return res.blob();
+      })
+      .then((image) => {
+        const ref = storage.ref(`camera/${Date.now()}.jpg`);
+        ref.put(image).then(() => {
+          console.log(ref.getDownloadURL);
+
+          ref.getDownloadURL().then((url) => {
+            console.log(url);
             this.setState({
-                permission:response.granted
-            })
-        })
-    }
+              photo: "",
+            });
+            this.props.savePhoto(url);
+          });
+        });
+      });
+  }
 
-    takePicture(){
-        if(!this.camera) return;
-        this.camera.takePictureAsync()
-        .then(photo => {
-            this.setState({
-                photo: photo.uri
-            })
-        })
-    }
+  onReject() {
+    this.setState({
+      photo: "",
+    });
+  }
 
-    uploadImage(){
-        fetch(this.state.photo)
-        .then(res => {
-            return res.blob();
-        })
-        .then(image => {
-            const ref = storage.ref(`camera/${Date.now()}.jpg`)
-            ref.put(image)
-            .then(() => {
-                console.log(ref.getDownloadURL);
-                
-                ref.getDownloadURL()
-                .then(url => {
-                    console.log(url);
-                    this.setState({
-                        photo:''
-                    })
-                    this.props.savePhoto(url)
-                })
-            })
-        })
-    }
+  render() {
+    console.log(this.state);
+    return (
+      <View style={styles.container}>
+        {this.state.photo ? (
+          <>
+            <Image style={styles.preview} source={{ uri: this.state.photo }} />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.reject}
+                onPress={() => this.onReject()}
+              >
+                <Text style={styles.text}>Tomar otra foto</Text>
+              </TouchableOpacity>
 
-    onReject(){
-        this.setState({
-            photo:''
-        })
-    }
-
-    render(){
-        console.log((this.state));
-        return(
-            <View style={styles.container}>
-                {
-                    this.state.photo ?
-                    <>
-                    <Image 
-                    style = {styles.preview}
-                    source={{uri:this.state.photo}}
-                    />
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity
-                        style={styles.reject}
-                        onPress={()=>this.onReject()}>
-                            <Text style={styles.text}>Tomar otra foto</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                        style={styles.accept}
-                        onPress={()=> this.uploadImage()}
-                        >
-                            <Text style={styles.text}>Me gusta esta foto</Text>
-                        </TouchableOpacity>
-                    </View>
-                </>
-                :
-                <Camera
-                style={styles.camera}
-                type={Camera.Constants.Type.front || Camera.Constants.Type.back}
-                ref= {referencia => this.camera = referencia}
-                >
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity
-                    style={styles.button}
-                    onPress={()=> this.takePicture()}>
-                    </TouchableOpacity>
-                </View>
-                </Camera>
-                }
+              <TouchableOpacity
+                style={styles.accept}
+                onPress={() => this.uploadImage()}
+              >
+                <Text style={styles.text}>Me gusta esta foto</Text>
+              </TouchableOpacity>
             </View>
-        )
-    }
-
+          </>
+        ) : (
+          <Camera
+            style={styles.camera}
+            type={Camera.Constants.Type.front || Camera.Constants.Type.back}
+            ref={(referencia) => (this.camera = referencia)}
+          >
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.takePicture()}
+              ></TouchableOpacity>
+            </View>
+          </Camera>
+        )}
+      </View>
+    );
+  }
 }
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    button: {
-        width: 124,
-        height: '100%',
-        borderWidth: 5,
-        borderColor: 'white',
-        borderRadius: 100,
-        backgroundColor: 'rgba(0,0,0,0.1)'
-    },
-    buttonContainer:{
-        width: '100%',
-        height: 125,
-        position: 'absolute',
-        bottom: 40,
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    text: {
-        color: "black",
-        fontSize: 15,
-        margin: 10,
-        fontFamily: 'Montserrat',
-        textAlign: 'center'
-    },
-    camera:{
-        flex: 1,
-        width: "100%",
-    },
-    reject:{
-        width: "40%",
-        height: 45,
-        backgroundColor: "#CCD5AE",
-        borderRadius: 30,
-        margin: 10,
-        alignItems: "center",
-    },
-    accept:{
-        width: "40%",
-        height: 45,
-        backgroundColor: "#CCD5AE",
-        borderRadius: 30,
-        margin: 10,
-        alignItems: "center",
-    },
-    preview:{
-        width: '100%',
-        flex: 6
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  button: {
+    width: 124,
+    height: "100%",
+    borderWidth: 5,
+    borderColor: "white",
+    borderRadius: 100,
+    backgroundColor: "rgba(0,0,0,0.1)",
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 125,
+    position: "absolute",
+    bottom: 40,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
+    fontSize: 15,
+    margin: 10,
+    fontFamily: "Montserrat",
+    textAlign: "center",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+  },
+  reject: {
+    width: "40%",
+    height: 45,
+    backgroundColor: "#CCD5AE",
+    borderRadius: 30,
+    margin: 10,
+    alignItems: "center",
+  },
+  accept: {
+    width: "40%",
+    height: 45,
+    backgroundColor: "#CCD5AE",
+    borderRadius: 30,
+    margin: 10,
+    alignItems: "center",
+  },
+  preview: {
+    width: "100%",
+    flex: 6,
+  },
+});
--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -1,114 +1,122 @@
-import React, {Component} from 'react';
-import { Text, Image, TextInput, TouchableOpacity, View, StyleSheet} from 'react-native';
-import { auth, db } from '../firebase/config';
-import MyCamera from '../components/MyCamera';
+import React, { Component } from "react";
+import {
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { auth, db } from "../firebase/config";
+import MyCamera from "../components/MyCamera";
 
 export default class CreatePost extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            comment: "",
-            photo: '',
-            showCamera: true,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: "",
+      photo: "",
+      showCamera: true,
+    };
+  }
 
-    handlePost(){
-        db.collection('posts').add({
-            owner: auth.currentUser.displayName,
-            description: this.state.comment,
-            email: auth.currentUser.email,
-            cratedAt: Date.now(),
-            likes: [],
-            comments: [],
-            photo: this.state.photo
-        })
+  handlePost() {
+    db.collection("posts")
+      .add({
+        owner: auth.currentUser.displayName,
+        description: this.state.comment,
+        email: auth.currentUser.email,
+        cratedAt: Date.now(),
+        likes: [],
+        comments: [],
+        photo: this.state.photo,
+      })
 
-        .then(response => {
-            console.log(response);
-            this.setState({
-                comment: '',
-            })
-            console.log(this.props);
-            this.props.navigation.navigate('Home')
-        })
-        .catch(error => {
-            console.log(error);
-            alert('Error en el posteo')
-        })
-    }
-
-    savePhoto(url){
+      .then((response) => {
+        console.log(response);
         this.setState({
-            photo: url,
-            showCamera:false
-        })
-    }
+          comment: "",
+        });
+        console.log(this.props);
+        this.props.navigation.navigate("Home");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error en el posteo");
+      });
+  }
 
-    render(){
-        return(
-            <>
-            {this.state.showCamera ?
-            <MyCamera savePhoto = {(url)=> this.savePhoto(url)}/>
-            :    
-            <View style={styles.container}>
-                <Image
-                    source={{uri: this.state.photo}}
-                    style = {styles.imagen}
-                />
-                <TextInput
-                    style= {styles.field}
-                    keyboardType='default'
-                    placeholder="Describí tu receta para motivar a otros chef de la comunidad"
-                    multiline={true}
-                    numberOfLines = {4}
-                    onChangeText={text => this.setState({ comment: text })}
-                    value = {this.state.comment}
-                />
-                <TouchableOpacity style = {styles.button} onPress={()=> this.handlePost()}>
-                    <Text style = {styles.text}> Post </Text>
-                </TouchableOpacity>
-            </View>
-            }
-            </>
-        )
-    }
+  savePhoto(url) {
+    this.setState({
+      photo: url,
+      showCamera: false,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.showCamera ? (
+          <MyCamera savePhoto={(url) => this.savePhoto(url)} />
+        ) : (
+          <View style={styles.container}>
+            <Image source={{ uri: this.state.photo }} style={styles.imagen} />
+            <TextInput
+              style={styles.field}
+              keyboardType="default"
+              placeholder="Describí tu receta para motivar a otros chef de la comunidad"
+              multiline={true}
+              numberOfLines={4}
+              onChangeText={(text) => this.setState({ comment: text })}
+              value={this.state.comment}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.handlePost()}
+            >
+              <Text style={styles.text}> Post </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: "white",
-    },
-    field: {
-        width: "90%",
-        height: 100,
-        backgroundColor: "#FAFAFA",
-        borderRadius: 30,
-        margin: 10,
-        padding: 10,
-        borderColor: "#CCD5AE",
-        borderWidth: 0.1,
-        fontFamily: 'Montserrat'
-    },
-    button: {
-        width: "40%",
-        height: 45,
-        backgroundColor: "#CCD5AE",
-        borderRadius: 30,
-        margin: 10,
-        alignItems: "center",
-    },
-    text: {
-        color: "black",
-        fontSize: 18,
-        margin: 10,
-    },
-    imagen:{
-        height: 500,
-        width: '90%',
-        marginTop:"3%",
-        marginBottom: "3%",
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  field: {
+    width: "90%",
+    height: 100,
+    backgroundColor: "#FAFAFA",
+    borderRadius: 30,
+    margin: 10,
+    padding: 10,
+    borderColor: "#CCD5AE",
+    borderWidth: 0.1,
+    fontFamily: "Montserrat",
+  },
+  button: {
+    width: "40%",
+    height: 45,
+    backgroundColor: "#CCD5AE",
+    borderRadius: 30,
+    margin: 10,
+    alignItems: "center",
+  },
+  text: {
+    color: "black",
+    fontSize: 18,
+    margin: 10,
+  },
+  imagen: {
+    height: 500,
+    width: "90%",
+    marginTop: "3%",
+    marginBottom: "3%",
+  },
+});
